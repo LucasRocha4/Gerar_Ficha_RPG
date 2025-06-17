cd98822239b63932edf2dfcbd21f70dcdf8e8b70
Lucas Rocha <lucasdanielrocha23@gmail.com>
2025-06-16 23:42:53 -0300

    front: paginas do home, criando e do login

--- app_gen_file_rpg/views.py
+++ app_gen_file_rpg/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import RegistrarUsuario
 
-# Create your views here.
+class RegistrarUsuario(CreateView):
+    form_class = RegistrarUsuario
+    template_name = 'registration/registrar.html'
+    success_url = reverse_lazy('login')
+
+def home(request):
+    return render(request, 'pages/home.html')
+
+def criando(request):
+    return render(request, 'pages/criando.html')
